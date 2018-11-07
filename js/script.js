3e2da0c4a869c7ce0d19edd982d1070230c5503a
mjablonska <martawaldon@gmail.com>
2018-11-07 12:49:52 +0100

    Added smooth scroll script in jquery

---
+++ js/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+  
+let width= window.innerWidth;    
+    
+    //hamburger menu 
+    
+    /*if(width<600) {                         //600px breakpoint in css
+        $('#menu').hide();                  
+        $("#hamburger").click(function(){
+            $('#menu').slideToggle();
+        });
+        
+        //hide menu after click 'home'//
+        
+         $('#home_menu').click(function() {  
+            $('#menu').slideUp();
+        });
+        
+    };*/
+    
+    //smooth scroll 
+    
+    function scroll(e) {
+
+    let href = $(this).attr('href');
+    e.preventDefault();
+
+    if(width < 600) {                      //600px breakpoint in css
+       $('html, body').animate({
+            scrollTop: $(href).offset().top -227
+        }, 800); 
+    } else {                        
+        $('html, body').animate({
+            scrollTop: $(href).offset().top - 50
+        }, 800);  
+    };
+
+    location.hash = href;
+    };
+
+    $('a[href*="#"]').click(scroll);
+    
+});
+    
