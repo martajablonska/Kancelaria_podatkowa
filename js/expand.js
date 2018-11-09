d95ecaa6bf133a683067e7619bf8d7a79b05fb32
mjablonska <martawaldon@gmail.com>
2018-11-09 14:50:57 +0100

    Added first part of expand functionality

--- js/expand.js
+++ js/expand.js
@@ -1,8 +1,45 @@
 const doradztwo = document.querySelector('#doradztwo');
-const btn = document.querySelector('#btn_doradztwo');
-const list = doradztwo.querySelector('.doradztwo_list');
+const btn = doradztwo.querySelector('.btn-show_more');
+const btnClose = doradztwo.querySelector('.btn-close');
+const container = document.querySelector('.doradztwo_container');
+
+
+
+const otherItems = [...document.querySelectorAll('.doradztwo_item')];
+
+const arrayBtn = [...document.querySelectorAll('.btn-show_more')];
+
+container.addEventListener('click', function(e) {
+    const wasClicked = e.target;
+    console.log(wasClicked);
+});
+
+
 
 btn.addEventListener('click', function() {
-    alert("hello!");
+    doradztwo.classList.add('show');
+    
+    otherItems.forEach(function(item) {
+        if(item !== doradztwo) {
+            item.classList.add('decrease');
+        }
+    });
 });
+
+btnClose.addEventListener('click', function() {
+    doradztwo.classList.remove('show');
+    
+    otherItems.forEach(function(item) {
+        if(item !== doradztwo) {
+            item.classList.remove('decrease');
+        }
+    });
+});
+
+
+
+/*const showMoreBtn = this.querySelector('.btn-show_more');
+const closeBtn = this.querySelector('.btn-close');*/
+
+
 
